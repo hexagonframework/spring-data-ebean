ec1dc051cb85b8e62ff0224f800660c44235c65f
OrDTesters <ordtesters@gmail.com>
2020-03-19 16:08:10 -0500

    Making test deleteById able to pass when run by itself

--- src/test/java/org/springframework/data/ebean/repository/UserRepositoryIntegrationTest.java
+++ src/test/java/org/springframework/data/ebean/repository/UserRepositoryIntegrationTest.java
@@ -99,6 +99,7 @@ public class UserRepositoryIntegrationTest {
 
   @Test
   public void deleteById() throws Exception {
+    user = new User("Xuegui", "Yuan", "yuanxuegui@163.com");
     userRepository.deleteById(1L);
     assertEquals(false, userRepository.findById(1L).isPresent());
 
